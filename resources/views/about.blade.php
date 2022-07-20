8d9bd05d23090f6a80250def5bf768ecc27dc58c
arif <ardwino88@gmail.com>
2022-07-20 19:07:19 +0700

    add: membuat view about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Larablog | About</title>
+</head>
+<body>
+	<center><h1>Ini adalah halaman About</h1></center>
+	<br>
+	<h3>{{ $name }}</h3>
+	<p>{{ $email }}</p>
+	<img src="img/{{ $image }}" alt="{{ $name }}">
+</body>
+</html>
