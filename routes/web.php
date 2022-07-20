d4fec546cabdd35aa25ba3ad7a4a60a7aaeff97c
arif <ardwino88@gmail.com>
2022-07-20 19:04:30 +0700

    add: menambah method baru

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Arif Dwi Nugroho",
+        "email" => "ardwiinoo@gmail.com",
+        "image" => "profil.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
