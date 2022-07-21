7437adfff938cc75ffd2bb8d45cce8dd624e3f2e
arif <ardwino88@gmail.com>
2022-07-21 18:12:35 +0700

    modif: mengubah tampilan about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.app');
+@extends('layouts.app')
 
-@section('content');
+@section('content')
 	<center><h1>Ini adalah halaman About</h1></center>
 	<br>
 	<h3>{{ $name }}</h3>
 	<p>{{ $email }}</p>
-	<img src="img/{{ $image }}" alt="{{ $name }}">
+	<img src="img/{{ $image }}" alt="{{ $name }}" class="img-thumbnail rounded-circle">
 @endsection
