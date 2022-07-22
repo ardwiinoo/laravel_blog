2168fffd555115b3d9bbefaf65d9e305045e94e9
arif <ardwino88@gmail.com>
2022-07-22 19:23:13 +0700

    modif: menambah atribut guarded

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,39 +2,13 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul post pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Arif Dwi N",
-            "body" => "Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Maxime, consequatur enim, necessitatibus ad incidunt omnis voluptatem minima, dolore, accusantium laudantium expedita ipsa totam. Autem voluptatibus cum obcaecati magnam excepturi sequi!Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod"
-        ],
-        [
-            "title" => "Judul post kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Nugroho",
-            "body" => "Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Maxime, consequatur enim, necessitatibus ad incidunt omnis voluptatem minima, dolore, accusantium laudantium expedita ipsa totam. Autem voluptatibus cum obcaecati magnam excepturi sequi!Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod"
-        ]
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts); // ubah array jadi collection, self --> property static
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all(); // static --> method static
-
-        // $post = [];
-        // foreach ($posts as $p) {
-        //     if ($p['slug'] === $slug) {
-        //         $post = $p;
-        //     }
-        // }
-
-        return $posts->firstWhere('slug', $slug);
-    }
+    //protected $fillable = ['title', 'excerpt', 'body']; // ini yang boleh diisi sisanya gaboleh
+    protected $guarded = ['id']; // ini yang gaboleh diisi sisanya boleh
 }
