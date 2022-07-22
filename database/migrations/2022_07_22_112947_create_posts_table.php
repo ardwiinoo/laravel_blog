f0a1afd7fbd40ffdf5e4279fb91c8c845d5b2e02
arif <ardwino88@gmail.com>
2022-07-22 19:27:52 +0700

    add: membuat tabel migrasi baru untuk post

---
+++ database/migrations/2022_07_22_112947_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title'); // judul
+            $table->string('slug')->unique(); // slug harus berbeda dari yang lain
+            $table->text('excerpt'); // untuk menyimpan sebagian kecil karakter.
+            $table->text('body'); // isi blog
+            $table->timestamp('published_at')->nullable(); // waktu post di publish
+            $table->timestamps(); // default untuk created_at dan updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
