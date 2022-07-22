bd7adf9eee5f6179a1c84005ae2e9acae3f83032
arif <ardwino88@gmail.com>
2022-07-22 19:25:37 +0700

    modif: mengganti link title dari id menjadi slug

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,9 +5,8 @@
 	
 	@foreach($posts as $post)
 		<article class="mb-5">
-			<h2><a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a></h2>
-			<small><b>{{ $post['author'] }}</b></small>
-			<p>{{ $post['body'] }}</p>
+			<h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
+			<p>{{ $post->excerpt }}</p>
 		</article>
 	@endforeach
 
