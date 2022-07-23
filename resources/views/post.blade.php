cc0669282e2dd14b05622cbbd441a24944e47b92
arif <ardwino88@gmail.com>
2022-07-23 19:41:01 +0700

    modif: menambah link nama author post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 	<article class="mb-5">
 			<h1>{{ $post->title }}</h1>
-			<p>By Arif Dwi Nugroho in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+			<p>By <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 			{!! $post->body !!} 
 	</article>
 
