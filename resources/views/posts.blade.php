15ba4c3dccb6fc0bf5871d55f87fd233465bdcd5
arif <ardwino88@gmail.com>
2022-07-23 19:41:14 +0700

    modif: menambah link nama author post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,10 +3,14 @@
 
 @section('content')
 	
+<h1 class="mb-5">Halaman Blog Posts</h1>
+
 	@foreach($posts as $post)
-		<article class="mb-5">
-			<h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h2>
+		<article class="mb-5 border-bottom">
+			<h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2>
+			<p>By <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 			<p>{{ $post->excerpt }}</p>
+			<a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
 		</article>
 	@endforeach
 
