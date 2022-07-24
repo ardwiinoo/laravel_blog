9287be135bc24b716755ec77a5c0511d6f3f4a69
arif <ardwino88@gmail.com>
2022-07-24 18:54:13 +0700

    modif: mengganti seed sebelumnya dengan seed faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Use Faker Library
+        User::factory(3)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
@@ -25,17 +26,17 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         // seed author
-        User::create([
-            'name' => 'Arif Dwi Nugroho',
-            'email' => 'ardwiinoo@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Arif Dwi Nugroho',
+        //     'email' => 'ardwiinoo@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
 
-        User::create([
-            'name' => 'Budi Sasangka',
-            'email' => 'sasangkabudi@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Budi Sasangka',
+        //     'email' => 'sasangkabudi@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
 
         // seed category
         Category::create([
@@ -49,41 +50,42 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // seed post
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vehicula sit amet tellus sed facilisis. Praesent quis mauris vitae augue egestas aliquet. Aenean lobortis, lectus nec commodo tincidunt,</p><p>lorem justo scelerisque enim, bibendum posuere lectus augue non nibh. Fusce non massa ut eros vestibulum malesuada. Sed risus ex, interdum ut lorem ac, rhoncus aliquam neque. Pellentesque non congue tortor. Duis non tincidunt turpis, sed tincidunt felis. Praesent quis auctor massa. Nam id ante a est ultrices malesuada. Vivamus vel velit fermentum, pellentesque risus ut, facilisis elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac tempor magna, vel ultrices ante.</p>',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        //     'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vehicula sit amet tellus sed facilisis. Praesent quis mauris vitae augue egestas aliquet. Aenean lobortis, lectus nec commodo tincidunt,</p><p>lorem justo scelerisque enim, bibendum posuere lectus augue non nibh. Fusce non massa ut eros vestibulum malesuada. Sed risus ex, interdum ut lorem ac, rhoncus aliquam neque. Pellentesque non congue tortor. Duis non tincidunt turpis, sed tincidunt felis. Praesent quis auctor massa. Nam id ante a est ultrices malesuada. Vivamus vel velit fermentum, pellentesque risus ut, facilisis elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac tempor magna, vel ultrices ante.</p>',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Kedua',
-            'slug' => 'judul-kedua',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vehicula sit amet tellus sed facilisis. Praesent quis mauris vitae augue egestas aliquet. Aenean lobortis, lectus nec commodo tincidunt,</p><p>lorem justo scelerisque enim, bibendum posuere lectus augue non nibh. Fusce non massa ut eros vestibulum malesuada. Sed risus ex, interdum ut lorem ac, rhoncus aliquam neque. Pellentesque non congue tortor. Duis non tincidunt turpis, sed tincidunt felis. Praesent quis auctor massa. Nam id ante a est ultrices malesuada. Vivamus vel velit fermentum, pellentesque risus ut, facilisis elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac tempor magna, vel ultrices ante.</p>',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        //     'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vehicula sit amet tellus sed facilisis. Praesent quis mauris vitae augue egestas aliquet. Aenean lobortis, lectus nec commodo tincidunt,</p><p>lorem justo scelerisque enim, bibendum posuere lectus augue non nibh. Fusce non massa ut eros vestibulum malesuada. Sed risus ex, interdum ut lorem ac, rhoncus aliquam neque. Pellentesque non congue tortor. Duis non tincidunt turpis, sed tincidunt felis. Praesent quis auctor massa. Nam id ante a est ultrices malesuada. Vivamus vel velit fermentum, pellentesque risus ut, facilisis elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac tempor magna, vel ultrices ante.</p>',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ketiga',
-            'slug' => 'judul-ketiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vehicula sit amet tellus sed facilisis. Praesent quis mauris vitae augue egestas aliquet. Aenean lobortis, lectus nec commodo tincidunt,</p><p>lorem justo scelerisque enim, bibendum posuere lectus augue non nibh. Fusce non massa ut eros vestibulum malesuada. Sed risus ex, interdum ut lorem ac, rhoncus aliquam neque. Pellentesque non congue tortor. Duis non tincidunt turpis, sed tincidunt felis. Praesent quis auctor massa. Nam id ante a est ultrices malesuada. Vivamus vel velit fermentum, pellentesque risus ut, facilisis elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac tempor magna, vel ultrices ante.</p>',
-            'category_id' => 1,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ketiga',
+        //     'slug' => 'judul-ketiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        //     'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vehicula sit amet tellus sed facilisis. Praesent quis mauris vitae augue egestas aliquet. Aenean lobortis, lectus nec commodo tincidunt,</p><p>lorem justo scelerisque enim, bibendum posuere lectus augue non nibh. Fusce non massa ut eros vestibulum malesuada. Sed risus ex, interdum ut lorem ac, rhoncus aliquam neque. Pellentesque non congue tortor. Duis non tincidunt turpis, sed tincidunt felis. Praesent quis auctor massa. Nam id ante a est ultrices malesuada. Vivamus vel velit fermentum, pellentesque risus ut, facilisis elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac tempor magna, vel ultrices ante.</p>',
+        //     'category_id' => 1,
+        //     'user_id' => 2
+        // ]);
 
 
-        Post::create([
-            'title' => 'Judul Keempat',
-            'slug' => 'judul-keempat',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vehicula sit amet tellus sed facilisis. Praesent quis mauris vitae augue egestas aliquet. Aenean lobortis, lectus nec commodo tincidunt,</p><p>lorem justo scelerisque enim, bibendum posuere lectus augue non nibh. Fusce non massa ut eros vestibulum malesuada. Sed risus ex, interdum ut lorem ac, rhoncus aliquam neque. Pellentesque non congue tortor. Duis non tincidunt turpis, sed tincidunt felis. Praesent quis auctor massa. Nam id ante a est ultrices malesuada. Vivamus vel velit fermentum, pellentesque risus ut, facilisis elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac tempor magna, vel ultrices ante.</p>',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Keempat',
+        //     'slug' => 'judul-keempat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        //     'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vehicula sit amet tellus sed facilisis. Praesent quis mauris vitae augue egestas aliquet. Aenean lobortis, lectus nec commodo tincidunt,</p><p>lorem justo scelerisque enim, bibendum posuere lectus augue non nibh. Fusce non massa ut eros vestibulum malesuada. Sed risus ex, interdum ut lorem ac, rhoncus aliquam neque. Pellentesque non congue tortor. Duis non tincidunt turpis, sed tincidunt felis. Praesent quis auctor massa. Nam id ante a est ultrices malesuada. Vivamus vel velit fermentum, pellentesque risus ut, facilisis elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ac tempor magna, vel ultrices ante.</p>',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
     }
 }
