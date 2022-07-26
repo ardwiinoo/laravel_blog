de8a27f76cd7fe29bf1d723d4b206ee9a9b717da
arif <ardwino88@gmail.com>
2022-07-26 21:01:29 +0700

    modif: membuat scope filter, membuat query berdasarkan pencarian

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,34 @@ class Post extends Model
     protected $guarded = ['id']; // ini yang gaboleh diisi sisanya boleh
     protected $with = ['category', 'user']; // eager loading
 
+    // untuk fitur search
+    public function scopeFilter($query, array $filters)
+    {
+        // if (isset($filters['search']) ? $filters['search'] : false) {
+        //     return $query->where('title', 'like', '%' . $filters['search'] . '%')->orWhere('body', 'like', '%' . $filters['search'] . '%'); // jika search diisi maka akan ditambahkan where sebelum latest()
+        // }
+
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            return $query->where('title', 'like', '%' . $search . '%')->orWhere('body', 'like', '%' . $search . '%'); // jika search diisi maka akan ditambahkan where sebelum latest()
+        });
+
+        $query->when($filters['category'] ?? false, function ($query, $category) {
+            return $query->whereHas('category', function ($query) use ($category) {
+                $query->where('slug', $category);
+            });
+        });
+
+        $query->when(
+            $filters['author'] ?? false,
+            fn ($query, $author) =>
+            $query->whereHas(
+                'user',
+                fn ($query) =>
+                $query->where('username', $author)
+            )
+        );
+    }
+
     // relasi post -- category (satu post hanya memiliki satu category) : 1 to 1 (belongsTo)
     public function category()
     {
