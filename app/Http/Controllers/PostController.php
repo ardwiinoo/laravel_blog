8d5f3aa9c3a97d2da2c00fc567f4f29baf5b2be4
arif <ardwino88@gmail.com>
2022-07-26 13:32:25 +0700

    modif: menambahkan kondisi untuk pencarian judul post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,11 +9,20 @@ class PostController extends Controller
 {
     public function index()
     {
+
+        // dd(request('search'));
+
+        $posts = Post::latest(); // ambil semua post urut terbaru
+
+        if (request('search')) {
+            $posts->where('title', 'like', '%' . request('search') . '%'); // jika search diisi maka akan ditambahkan where sebelum latest()
+        }
+
         return view('posts', [
             "title" => "All Posts",
             "active" => "Blog",
             // "posts" => Post::all() // menampilkan semua post urut dari id
-            "posts" => Post::latest()->get() // menampilkan post urut dari waktu terbaru dengan eager loading
+            "posts" => $posts->get() // menampilkan post urut dari waktu terbaru dengan eager loading
         ]);
     }
 
