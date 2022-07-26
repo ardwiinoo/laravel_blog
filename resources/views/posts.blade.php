71e6546bf0ca3c2467cb90823da290b5481e892f
arif <ardwino88@gmail.com>
2022-07-26 13:33:03 +0700

    modif: mengubah kondisi else dan menambah form pencarian judul post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,18 @@
 
 @section('content')
 	
-<h1 class="mb-5">{{ $title }}</h1>
+<h1 class="mb-3 text-center">{{ $title }}</h1>
+
+<div class="row justify-content-center mb-3">
+	<div class="col-md-6">
+		<form action="/blog">
+			<div class="input-group mb-3">
+				<input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
+				<button class="btn btn-success" type="submit">Search</button>
+			  </div>
+		</form>
+	</div>
+</div>
 
 
 @if ($posts->count()) <!-- true: jika > 0; dan false: jika < 0; -->
@@ -21,31 +32,32 @@
 		<a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-success">Read more</a>
 		</div>
 	</div>
+
+	<div class="container">
+		<div class="row">
+		@foreach($posts->skip(1) as $post)
+			<div class="col-md-4 mb-3">
+				<div class="card shadow-sm p-1 mb-5 bg-body rounded">
+					<div class="position-absolute text-white px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
+					<img src="https://source.unsplash.com/300x200?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}" loading="lazy">
+					<div class="card-body">
+					<h5 class="card-title">{{ $post->title }}</h5>
+					<p><small class="text-muted"> 
+						By <a href="/authors/{{ $post->user->username }}" class="text-decoration-none">{{ $post->user->name }}</a> 
+						{{ $post->created_at->diffForHumans() }} <!-- diffForHumans() akan menampilkan waktu format 3 min ago (library carbon) -->
+						</small></p>
+					<p class="card-text">{{ $post->excerpt }}</p>
+					<a href="/posts/{{ $post->slug }}" class="btn btn-success">Read more</a>
+					</div>
+				</div>
+			</div>
+		@endforeach
+		</div>
+	</div>
+
 @else
 	<p class="text-center fs-4">Tidak ada post yang ditemukan.</p>
 @endif
-
-<div class="container">
-	<div class="row">
-	@foreach($posts->skip(1) as $post)
-		<div class="col-md-4 mb-3">
-			<div class="card shadow-sm p-1 mb-5 bg-body rounded">
-				<div class="position-absolute text-white px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
-				<img src="https://source.unsplash.com/300x200?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}" loading="lazy">
-				<div class="card-body">
-				  <h5 class="card-title">{{ $post->title }}</h5>
-				  <p><small class="text-muted"> 
-					By <a href="/authors/{{ $post->user->username }}" class="text-decoration-none">{{ $post->user->name }}</a> 
-					{{ $post->created_at->diffForHumans() }} <!-- diffForHumans() akan menampilkan waktu format 3 min ago (library carbon) -->
-					</small></p>
-				  <p class="card-text">{{ $post->excerpt }}</p>
-				  <a href="/posts/{{ $post->slug }}" class="btn btn-success">Read more</a>
-				</div>
-			  </div>
-		</div>
-	@endforeach
-	</div>
-</div>
 
 
 @endsection
