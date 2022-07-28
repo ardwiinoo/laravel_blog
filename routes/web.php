e3dd3ef3b548fab3a80909054be68c055c1d0dbd
arif <ardwino88@gmail.com>
2022-07-28 20:47:45 +0700

    add: membuat resource route untuk DashboardPostController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+
+use App\Http\Controllers\DashboardPostController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
@@ -73,4 +74,9 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth'); // hanya bisa diakses user yang sudah authentikasi
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+})->middleware('auth'); // hanya bisa diakses user yang sudah authentikasi
+
+// controller otomatis kelola data crud
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
