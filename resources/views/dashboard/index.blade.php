fce95973be6fc9b77e3a5b47e83bff4681eff248
arif <ardwino88@gmail.com>
2022-07-28 20:47:00 +0700

    modif: mengubah tampilan dashboard index

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,7 @@
-<h1>Welcome, Arif</h1>
+@extends('dashboard.layouts.app')
+
+@section('content_dashboard')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Welcome back, {{ auth()->user()->name }}</h1>
+    </div>
+@endsection
