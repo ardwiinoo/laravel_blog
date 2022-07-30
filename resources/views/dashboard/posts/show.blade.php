677c116f2d6ef5fd93fd57b016734df62c50e8ac
arif <ardwino88@gmail.com>
2022-07-30 21:10:41 +0700

    modif: membuat percabangan untuk image thumnail

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -8,11 +8,21 @@
 			<h1 class="mb-3">{{ $post->title }}</h1>
 			
             <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all my post</a>
-            <a href="#" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
-            <a href="#" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
+            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+            <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+				@method('delete')
+				@csrf
+				<button class="btn btn-danger" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span> Delete</button>
+			  </form>
 			
-			<img src="https://source.unsplash.com/1200x600?{{ $post->category->name }}" class="img-fluid rounded mt-3" alt="{{ $post->category->name }}">
-
+			  @if ($post->image)
+			  	<div style="max-height: 600px; overflow: hidden">
+					<img src="{{ asset('storage/' . $post->image) }}" class="img-fluid rounded mt-3" alt="{{ $post->category->name }}">
+				</div>
+			  @else
+			  	<img src="https://source.unsplash.com/1200x600?{{ $post->category->name }}" class="img-fluid rounded mt-3" alt="{{ $post->category->name }}">
+			  @endif
+			
 			<article class="my-3 fs-6 content-justify">
 				{!! $post->body !!} 
 			</article>
