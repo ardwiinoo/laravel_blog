45e93b18516fa6e62cd25dc966733c07a826b68b
arif <ardwino88@gmail.com>
2022-07-30 19:34:39 +0700

    modif: menambah margin pada form create new post

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -6,28 +6,45 @@
     </div>
 
    <div class="col-lg-8">
-    <form action="/dashboard/posts" method="post"> <!-- otomatis mengarah ke method store -->
+    <form action="/dashboard/posts" method="post" class="mb-5"> <!-- otomatis mengarah ke method store -->
         @csrf
 
         <div class="mb-3">
           <label for="title" class="form-label">Title</label>
-          <input type="text" class="form-control" id="title" name="title">
+          <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" autofocus value="{{ old('title') }}">
+          @error('title')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+          @enderror
         </div>
         <div class="mb-3">
             <label for="slug" class="form-label">Slug</label>
-            <input type="text" class="form-control" id="slug" name="slug" disabled readonly>
+            <input type="text" id="slug" class="form-control @error('slug') is-invalid @enderror" name="slug" readonly value="{{ old('slug') }}">
+            @error('slug')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+          @enderror
         </div>
         <div class="mb-3">
             <label for="category" class="form-label">Category</label>
             <select class="form-select" name="category_id">
                 @foreach ($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @if (old('category_id') == $category->id)
+                        <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+                    @else
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endif
                 @endforeach
             </select>
         </div>
         <div class="mb-3">
             <label for="body" class="form-label">Body</label>
-            <input id="body" type="hidden" name="body">
+            @error('body')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
+            <input id="body" type="hidden" name="body" value="{{ old('body') }}">
             <trix-editor input="body"></trix-editor>
         </div>
 
@@ -42,7 +59,7 @@
         const slug = document.querySelector('#slug');
         
         title.addEventListener('change', function() {
-            fetch('/dashboard/posts/checkSlug?title=' + title.value)
+            fetch(`/dashboard/posts/checkSlug?title=${title.value}`)
             .then(response => response.json())
             .then(data => slug.value = data.slug)
         });
